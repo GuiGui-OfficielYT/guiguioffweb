10a196661bac70a962506094828a23e0f5b2a67e
GuiGui Officiel <62442697+GuiGui-OfficielYT@users.noreply.github.com>
2022-08-30 13:35:14 -0400

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,9 @@
+import Head from 'next/head'
+import Header from '@components/Header'
+import Footer from '@components/Footer'
+
+export default function Home() {
+  return (
 <!DOCTYPE html>
 
 <title>GuiGui Officiel</title>
@@ -49,3 +55,6 @@
 
 </body>
 </html>
+
+  )
+}
